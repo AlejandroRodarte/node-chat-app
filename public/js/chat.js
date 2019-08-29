3261151aea294fcee9242825ee25b8fd95bcb2ba
Alejandro Rodarte <alejandrorodarte1@gmail.com>
2019-08-28 20:58:40 -0600

    Lecture 155: Socket.io Events

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,3 +1,20 @@
 // we can access socket.io methods since we declared the /socket.io/socket.io.js file
 // on index.html
-io();
+
+// the client socket object
+const socket = io();
+
+// the server fires of an 'updatedCount' event, so we use socket.on() to register such event and
+// we can react to it as clients
+// the 'updatedCount' event emits a 'count' number, so we receive it as an argument in the callback
+socket.on('countUpdated', (count) => {
+    console.log('The count has been updated!' + count);
+});
+
+document.querySelector('#increment').addEventListener('click', (e) => {
+
+    // use socket.emit() to emit an event from the client so the server can respond and execute some code
+    // from the server side
+    socket.emit('increment');
+
+});
--- src/index.js
+++ src/index.js
@@ -21,8 +21,29 @@ const publicDir = path.join(__dirname, '..', 'public');
 // set the dir for Express to acknowledge
 app.use(express.static(publicDir));
 
-io.on('connection', () => {
-    console.log('New WebSocket connection.');
+let count = 0;
+
+// connection() callback runs each time a new client is connected
+// where the 'socket' argument contains information about that client connection
+io.on('connection', (socket) => {
+
+    // similar to Angular, emit() a 'countUpdated' for the subscribers (clients) to listen to
+    // and run some code on their side
+    // the second argument is the data to send with the event trigger and we can access it on
+    // the client callback function in its on() method
+    socket.emit('countUpdated', count);
+
+    // socket.on() to listen when the particular client socket emits an 'increment' event
+    socket.on('increment', () => {
+
+        // increment the count
+        count++;
+
+        // emit the new count value to ALL clients with io.emit()
+        io.emit('countUpdated', count);
+
+    });
+
 });
 
 // make the server listen on the assigned port
