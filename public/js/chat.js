6a2ba7c32cfe5e0582fa8a44390cc8380c5bb9f3
Alejandro Rodarte <alejandrorodarte1@gmail.com>
2019-08-28 20:25:59 -0600

    Lecture 154: Getting Started with Socket.io

---
+++ public/js/chat.js
@@ -0,0 +1,3 @@
+// we can access socket.io methods since we declared the /socket.io/socket.io.js file
+// on index.html
+io();
--- src/index.js
+++ src/index.js
@@ -1,7 +1,16 @@
 const express = require('express');
+const http = require('http');
 const path = require('path');
+const socketio = require('socket.io');
 
+// express app
 const app = express();
+
+// use the Express app to create an http server
+const server = http.createServer(app);
+
+// bind socket.io with the http server to enable WebSockets
+const io = socketio(server);
 
 // use environment port (when deploying on Heroku) or port 3000 if it's undefined
 const port = process.env.PORT || 3000;
@@ -12,6 +21,11 @@ const publicDir = path.join(__dirname, '..', 'public');
 // set the dir for Express to acknowledge
 app.use(express.static(publicDir));
 
-app.listen(port, () => {
+io.on('connection', () => {
+    console.log('New WebSocket connection.');
+});
+
+// make the server listen on the assigned port
+server.listen(port, () => {
     console.log(`Server up and running in port ${port}.`);
 });
